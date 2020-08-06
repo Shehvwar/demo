c7e261f6eef07c4b70500885b3eff56da08ace57
shehvwar <shehvwars12@gmail.com>
2020-08-06 15:14:27 +0530

    Updates

---
+++ precache-manifest.967d1702c0a1492e0887ca4b923a6728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cad50bf1e48e966994dd8f6057c34b",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "d98cc449e8468be96fbd",
+    "url": "/demo/static/css/main.8d863e9f.chunk.css"
+  },
+  {
+    "revision": "3bac9bd6bd4b7ae63c75",
+    "url": "/demo/static/js/2.f295bb26.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/demo/static/js/2.f295bb26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98cc449e8468be96fbd",
+    "url": "/demo/static/js/main.ce702ad3.chunk.js"
+  },
+  {
+    "revision": "49d9e13ee19588b4d46e",
+    "url": "/demo/static/js/runtime-main.7018aca8.js"
+  }
+]);
